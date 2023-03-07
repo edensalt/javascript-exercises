6f24bd0ea65d8df4ad6d68ef10cf2a136cb6a5bc
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-07 13:54:49 -0600

    Add conversion code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = ((temp - 32) * (5/9));
+  console.log(typeof newTemp);
+  let newTempOneDecimal = parseFloat(newTemp.toFixed(1));
+  return newTempOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = (((9/5) * temp) + 32);
+  let newTempOneDecimal = parseFloat(newTemp.toFixed(1));
+  return newTempOneDecimal;
 };
 
 // Do not edit below this line
