3345f383da2f9a0f32ba1f48c069ea015801d893
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-06 21:55:48 -0600

    Write repeat function to pass test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    let result = '';
+    for (let i = 0; i < 3; i++) {
+        result += a;
+    }
+    return result;
 };
-
+repeatString('hey', 3) // returns 'heyheyhey'
 // Do not edit below this line
 module.exports = repeatString;
+
