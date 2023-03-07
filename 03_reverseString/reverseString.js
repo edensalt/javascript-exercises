81eacc108ae49dccb0d558d7feabf6f2a63e2468
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-06 22:54:52 -0600

    Update script to solve for tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let word = '';
+    for (i = string.length - 1; i>=0; i--) {  // While the string still has letters in it (i.e. longer than it's full length)
+        word += string.slice((i),(i+1));  // Slice off the last letter and add it to a new element
+    }
+    return word;
 };
 
 // Do not edit below this line
