ec97cc5b2d09cdb2b746009c39fd1b65edb942d0
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-07 14:27:57 -0600

    Init

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+    // split the word into an array
+
+    let palTest = word.toLowerCase().split("");
+
+    // make the forward word just letters
+
+    let palForward = palTest.join("");
+
+    // make the backward word just letters, reversed
+
+    let palBackward = palTest.reverse().join("");
+
+    if (palForward === palBackward) {
+        return true;
+    } else return false;
 
 };
 
