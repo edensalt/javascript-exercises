2a8a29a85281b2168abf2c2dd9b76f95dbb1d3b6
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-08 10:55:35 -0600

    Init

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(entry) {
+
+    // if number is string, convert to int
+
+    let num = +entry;
+
+    console.log(typeof num);
+
+    // if number is negative, return error
+
+    if (num < 0) return "OOPS";
+
+    // set sequence array
+
+    const sequence = new Array (1, 1);
+
+
+    // run loop to get location of sequence
+
+    for (let i = 2; i < num; i++) {
+        let addNum = sequence[sequence.length -2] + sequence[sequence.length -1];
+        sequence.push(addNum);
+    }
+
+    return sequence[sequence.length-1];
 
 };
 
