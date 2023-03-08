29e4fca00c7699ff20d2cb216d11613078632297
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-08 11:39:35 -0600

    Init and pass first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // run a loop to calculate age
+
+    people.forEach(person => {
+        if (typeof person.yearOfDeath === undefined) {
+            const today = new Date();
+            const year = today.getFullYear();
+            age = year - person.yearOfBirth;
+        } else age = person.yearOfDeath - person.yearOfBirth;
+        person.age = age;
+    })
+
+    console.log(people);
+
+    // sort the array to find the oldest person
+
+    const ageSort = people.sort(function(a, b){return b.age - a.age});
+
+    console.log(people);
+
+    return ageSort[0];
 };
 
 // Do not edit below this line
