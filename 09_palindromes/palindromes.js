714ab95c3b324f1029bcd591a4a6deef9e92d516
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-13 21:30:45 -0500

    Use objects instead of for loops

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,42 +1,8 @@
 const palindromes = function (word) {
-
-    // remove non-letters from the word
-
-    let lettersOnly = word.replaceAll(/[^a-zA-Z]/g, "");
-
-    console.log(lettersOnly);
-
-    // make the word all lowercase
-
-    let forwardLower = lettersOnly.toLowerCase();
-
-    console.log(forwardLower);
-    
-    // split the word into an array
-
-    let forwardArray = forwardLower.split("");
-
-    console.log(forwardArray);
-
-    // reverse the array
-
-    let backwardArray = forwardArray.reverse();
-
-    console.log(backwardArray);
-
-    // join the array into a backward word
-
-    let backwardLower = backwardArray.join("");
-
-    console.log(backwardLower);
-
-    // check if backward and forward are the same
-
-    if (forwardLower === backwardLower) {
-        return true;
-    } else return false;
-
-}
+    const wordForward = word.toLowerCase().replaceAll(/[^a-zA-Z]/g, "");
+    console.log(wordForward);
+    return wordBackward = wordForward.split("").reverse().join("") == wordForward;
+};
 
 // Do not edit below this line
 module.exports = palindromes;
